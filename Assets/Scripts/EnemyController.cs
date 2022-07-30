f86aa5016c9772307e15aa6d5baf1f48fc70e99b
Akchy <akarshashok12@gmail.com>
2022-07-30 17:46:31 +0530

    Completed Project

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class EnemyController : GazePointer
+{
+    public GameObject Skeleton;
+    Vector3 endPos;
+    public float speed;
+    int _limit =1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        endPos = (transform.position - Vector3.zero).normalized;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, endPos, speed * Time.deltaTime );
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if(other.CompareTag("Player"))
+            Attack();
+    }
+
+    public override void OnPointerDown(PointerEventData eventData){
+        base.OnPointerDown(eventData);
+        //Debug.Log("hello Davish " + _limit);
+        if(_limit!=1){
+            PlayerManager.currentScore +=100;
+            Death();
+        }
+        else
+            _limit = 2;
+    }
+
+    public void Death(){
+        Debug.Log("death Davish");
+        Skeleton.GetComponent<Animator>().SetTrigger("die");
+        Skeleton.transform.SetParent(null);
+        Destroy(gameObject);
+    }
+
+    public void Attack(){
+        Skeleton.GetComponent<Animator>().SetTrigger("attack");
+        PlayerManager.playerHealth -= 0.2f;
+    }
+}
---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class PlayerManager : MonoBehaviour
+{
+    public static float playerHealth, currentScore;
+    public Image healthImg;
+    public TMP_Text scoreText;
+    public GameObject gameOn, gameOff, enemies;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(playerHealth <= 0)
+            GameOver();
+        healthImg.fillAmount = playerHealth;
+        scoreText.text = currentScore.ToString();
+        
+    }
+
+    public void GameStart(){
+        gameOn.SetActive(true);
+        gameOff.SetActive(false);
+        playerHealth = 1;
+        currentScore = 0;
+        scoreText.text = "0";
+    }
+
+    public void GameOver(){
+        foreach  (Transform child in enemies.transform)
+            child.gameObject.GetComponent<EnemyController>().Death();
+        gameOn.SetActive(false);
+        gameOff.SetActive(true);
+    }
+}
